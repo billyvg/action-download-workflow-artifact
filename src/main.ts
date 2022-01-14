382c03f223cbc443c6ed7533e4cd180b116391ce
Billy Vong <billyvg@users.noreply.github.com>
2022-01-14 15:19:59 -0500

    feat: initial feature (#3)
    Download artifact from another branch and/or workflow.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,61 @@
+import * as github from '@actions/github';
 import * as core from '@actions/core';
-import {wait} from './wait';
+import {getArtifactsForBranchAndWorkflow} from './getArtifactsForBranchAndWorkflow';
+import {download} from './download';
 
 async function run(): Promise<void> {
+  const token = core.getInput('github-token');
+  const octokit = token && github.getOctokit(token);
+
+  if (!octokit) {
+    throw new Error('`inputs.github-token` missing');
+  }
+
+  //
+  // Get inputs
+  //
+  const {owner, repo} = github.context.repo;
+  const branch = core.getInput('branch');
+  const workflowName =
+    core.getInput('workflow') || process.env.GITHUB_WORKFLOW || '';
+  const artifactName = core.getInput('artifact');
+  const downloadPath = core.getInput('path');
+
+  if (!artifactName) {
+    throw new Error('No artifact name provided');
+  }
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // Need to do a bunch of API calls to actually find artifacts for this
+    // workflow that have previously been run (e.g. on a different branch)
+    const resp = await getArtifactsForBranchAndWorkflow(octokit, {
+      owner,
+      repo,
+      branch,
+      workflowName,
+      artifactName,
+    });
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    if (!resp?.artifact) {
+      core.setFailed('Unable to find artifact');
+      return;
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    core.debug(`Artifact url: ${resp?.artifact.url}`);
+
+    await download(octokit, {
+      owner,
+      repo,
+      artifactId: resp.artifact.id,
+      artifactName,
+      downloadPath,
+    });
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (!(error instanceof Error)) {
+      return;
+    }
+
+    core.setFailed(error.message);
   }
 }
 
